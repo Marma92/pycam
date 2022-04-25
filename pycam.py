1f149e2cb5d6ada16d586026856af2f33d200612
Amram ELBAZ <elbaz.amram@gmail.com>
2022-04-25 21:06:01 +0200

    starting to write specs

--- pycam.py
+++ pycam.py
@@ -3,6 +3,20 @@ from time import sleep
 
 cam = PiCamera()
 
+cam.annotate_text = "BONJOUR MONDE"
 cam.start_preview()
 sleep(20)
 cam.stop_preview()
+
+# v1 specs :
+# start the camera in manual mode :
+#   - Ability to set speed and ISOs
+#   - Ability to set white balance
+# Aperture priority mode :
+#   - Aperture-based automatic shutter speed
+# Settings:
+#   - Ability to set image resolution
+#   - Ability to set path
+# natural previewer (no ATH)
+# detailed previewer (with ATH)
+# ez gallery ?
