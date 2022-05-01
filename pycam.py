1d889245b2bc4407e8090516610c203adf4452ff
Amram ELBAZ <elbaz.amram@gmail.com>
2022-05-01 16:39:30 +0200

    updating specs

--- pycam.py
+++ pycam.py
@@ -10,13 +10,14 @@ cam.stop_preview()
 
 # v1 specs :
 # start the camera in manual mode :
-#   - Ability to set speed and ISOs
+#   - Ability to set shutter speed and ISOs
 #   - Ability to set white balance
 # Aperture priority mode :
 #   - Aperture-based automatic shutter speed
 # Settings:
 #   - Ability to set image resolution
-#   - Ability to set path
+#   - Ability to set sharpness
 # natural previewer (no ATH)
 # detailed previewer (with ATH)
+# adding a 3/3 canvas on the previewer
 # ez gallery ?
